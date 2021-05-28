20afb896e7baed58353ebb44e4ce7613c4766a18
Ranjit Barsa <barsaranjeet15@gmail.com>
2021-05-28 15:06:04 +0530

    changes in simplewebsocket

--- simpleWebSocket.js
+++ simpleWebSocket.js
@@ -8,33 +8,39 @@ app.ws('/*', {
     maxPayloadLength: 16 * 1024 * 1024,
     idleTimeout: 10,
     maxBackpressure: 1024,
-  
+
     /* Handlers */
     open: (ws) => {
-      /* Let this client listen to all sensor topics */
-      console.log("client connected!!")
-      ws.subscribe('home/sensors/#');
+        /* Let this client listen to all sensor topics */
+        console.log("client connected!!")
     },
     message: (ws, message, isBinary) => {
         var messageString = new TextDecoder("utf-8").decode(message)
-        setInterval(function(){ 
-            ws.send(new Date().toString())
-         }, 1000);   
+
+        var interval = setInterval(function () {
+            try {
+                ws.send(new Date().toString())
+            } catch (error) {
+                clearInterval(interval)
+            }
+
+        }, 1000);
         ws.send("Sending date!!")
+
     },
     drain: (ws) => {
-  
+
     },
     close: (ws, code, message) => {
-      /* The library guarantees proper unsubscription at close */
-      console.log("client disconnected!!")
+        /* The library guarantees proper unsubscription at close */
+        console.log("client disconnected!!")
     }
-  }).any('/*', (res, req) => {
+}).any('/*', (res, req) => {
     res.end('Nothing to see here!');
-  }).listen(port, (token) => {
+}).listen(port, (token) => {
     if (token) {
-      console.log('Listening to port ' + port);
+        console.log('Listening to port ' + port);
     } else {
-      console.log('Failed to listen to port ' + port);
+        console.log('Failed to listen to port ' + port);
     }
-  });
+});
